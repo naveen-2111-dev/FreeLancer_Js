aa6e53e6ec18c4380da5fa5d4242a25a2e536c0c
27janani <sce22is056@sairamtap.edu.in>
2024-11-21 14:35:43 +0530

    about

---
+++ pages/About.js
@@ -0,0 +1,14 @@
+// pages/About.js
+
+import React from 'react';
+
+const About = () => {
+  return (
+    <div>
+      <h1>About Page</h1>
+      <p>This is the about page content.</p>
+    </div>
+  );
+};
+
+export default About;  // Ensure you are exporting the component as default
