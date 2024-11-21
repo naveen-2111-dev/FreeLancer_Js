5830472644e5155c35b2748ac225a23d4ed16c88
27janani <sce22is056@sairamtap.edu.in>
2024-11-21 15:14:44 +0530

    signup api

--- pages/Api/signup.js
+++ pages/Api/signup.js
@@ -1,12 +1,33 @@
-export default async function POST(req,res){
-    try{
-        const {firstname,lastname, email, password, country} = req.body;
+import Connect from "@/lib/connect";
+import bcrypt from "bcryptjs";
+
+export default async function POST(req, res) {
+    try {
+        const { firstname, lastname, email, password, country } = req.body;
+
         if (!firstname || !lastname || !email || !password || !country) {
             return res.status(400).json({ error: "Missing required user fields" });
         }
 
-        
-    }catch(err){
+        const hashedPassword = await bcrypt.hash(password, 10); 
+        const collection = await Connect();
+        const existingUser = await collection.findOne({ email });
+        if (existingUser) {
+            return res.status(409).json({ error: "Email already exists" });
+        }
 
+        const newUser = {
+            firstname,
+            lastname,
+            email,
+            password: hashedPassword, 
+            country,
+        };
+
+        await collection.insertOne(newUser);
+        return res.status(201).json({ message: "User successfully created" });
+    } catch (err) {
+        console.error("Error during sign-up:", err);
+        return res.status(500).json({ error: "Internal server error" });
     }
 }
